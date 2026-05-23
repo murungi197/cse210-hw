3bd5746170686bdc27383cd8a6aa0de8ead58ba9
murungi197 <murungiinnocent197@gmail.com>
2026-05-23 22:43:27 +0100

    week 3

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("John", 3, 16);
+
+        Scripture scripture = new Scripture(
+            reference,
+            "For God so loved the world that he gave his only begotten Son"
+        );
+
+        while (true)
+        {
+            Console.Clear();
+            scripture.Display();
+
+            if (scripture.AllHidden())
+            {
+                break;
+            }
+
+            Console.WriteLine("\nPress Enter or type 'quit':");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            scripture.HideRandomWords(3); // hides 3 words each time
+        }
     }
 }
